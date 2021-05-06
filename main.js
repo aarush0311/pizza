b4d5187c47be92bf0762bf5c82d24194478edbb5
aarush0311 <72295580+aarush0311@users.noreply.github.com>
2021-05-06 17:34:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,24 +6,25 @@ menu_list_array = [
 "special pizzaria offer"
 ];
 
-function getMenu(){
+function getmenu(){
     var htmlData;
     htmlData="<o1 class='menulist'>"
     menu_list_array.sort();
-    for(var i=0;i<menu_list_array.lenght;i++){
-        htmldata=htmldata+"</ol>"
-        document.getElementById("display_menu").innerHTML=htmldata;
+    for(var i=0;i<menu_list_array.length;i++){
+       htmlData=htmlData+'<li>'+menu_list_array[i]+'</li>';
     }
+    htmlData=htmlData+"</ol>"
+    document.getElementById("display_menu").innerHTML=htmlData;
 }
 function add_item(){
-    var htmldata;
+    var htmlData;
     var item=document.getElementById("add_item").value;
     menu_list_array.push(item);
     menu_list_array.sort();
-    htmldata="<section class='card'>"
-    for(var i=0;i<menu_list_array.lenght;i++){
-        htmldata=htmldata+'<div class="card">'+'img src="image/pizzaImg.png"/'+menu_list_array[i]+'</div>'
+    htmlData="<section class='card'>"
+    for(var i=0;i<menu_list_array.length;i++){
+        htmlData=htmlData+'<div class="card">'+'<img src="pizzaImg.png"/>'+menu_list_array[i]+'</div>'
     }
-    htmldata=htmldata+"</section>"
-    document.getElementById("display_addedmenu").innerHTML=htmldata;
+    htmlData=htmlData+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmlData;
 }
